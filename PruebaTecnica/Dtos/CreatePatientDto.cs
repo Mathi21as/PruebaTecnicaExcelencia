14335c655e5aa787fdf73f39ce144e719991bb21
Mathias Ledesma <romanmathias21@gmail.com>
2024-02-22 23:03:11 -0300

    first commit

---
+++ PruebaTecnica/Dtos/CreatePatientDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica.Dtos
+{
+    public class CreatePatientDto
+    {
+        [Required (ErrorMessage = "El nombre es obligatorio.")]
+        public string firstname { get; set; }
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        public string lastname { get; set; }
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [RegularExpression("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$", 
+            ErrorMessage = "El email no es valido.")]
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+
+    }
+}
---
+++ PruebaTecnica/Entities/PatientsdbEntity.cs
@@ -0,0 +1,29 @@
+﻿using PruebaTecnica.Dtos;
+
+namespace PruebaTecnica.Entities
+{
+    public class PatientsdbEntity
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string dni { get; set; }
+
+        public PatientDto toDto()
+        {
+            return new PatientDto()
+            {
+                id = id,
+                name = name,
+                lastname = lastname,
+                email = email,
+                phone = phone,
+                address = address,
+                dni = dni
+            };
+        }
+    }
+}
