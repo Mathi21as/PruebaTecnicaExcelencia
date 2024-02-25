b75b3d83d8abef5d3ea717eed9105b7bbed38149
Mathias Ledesma <romanmathias21@gmail.com>
2024-02-25 02:20:55 -0300

    environment variables config added

---
+++ PacientesCRUD/DotEnv.cs
@@ -0,0 +1,27 @@
+﻿namespace PacientesCRUD
+{
+    using System;
+    using System.IO;
+
+    public static class DotEnv
+    {
+        static string filePath = "/.env.local";
+        public static void Load()
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+
+                Environment.SetEnvironmentVariable(parts[0], parts[1]);
+            }
+        }
+    }
+}
--- PacientesCRUD/Program.cs
+++ PacientesCRUD/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using PacientesCRUD;
 using PacientesCRUD.Models;
 
+DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRouting(routing => routing.LowercaseUrls = true);
 builder.Services.AddDbContext<PatientDatabaseContext>(builder =>
 {
-    builder.UseMySql($"server=localhost;port=3306;database=patientsdb;uid=root;password=root1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
+    builder.UseMySql($"server=localhost;port=3306;database=patientsdb;uid={Environment.GetEnvironmentVariable("MYSQL_USER")};password={Environment.GetEnvironmentVariable("MYSQL_PASSWORD")}", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
 });
 
 var app = builder.Build();
